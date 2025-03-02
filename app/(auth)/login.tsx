b1dea972ceba62dbda91102598aa83e3f0ff9f1c
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2025-03-02 21:36:42 +0300

    feat: implement Login and Register components with form validation and navigation

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Pressable, Alert } from "react-native";
+import React, { useRef, useState } from "react";
+import { useRouter } from "expo-router";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Input from "@/components/Input";
 import Typo from "@/components/Typo";
+import Button from "@/components/Button";
 import { colors, spacingY, spacingX } from "@/constants/theme";
 import { verticalScale } from "@/utils/styling";
 import BackButton from "@/components/BackButton";
 import * as Icons from "phosphor-react-native";
 
 const Login = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+    if (!emailRef.current || !passwordRef.current) {
+      Alert.alert("Login", "Please fill all the fields");
+      return;
+    }
+    console.log("email", emailRef.current);
+    console.log("password", passwordRef.current);
+    console.log("good to go");
+  };
   return (
     <ScreenWrapper>
       <View style={styles.container}>
@@ -16,7 +32,7 @@ const Login = () => {
 
         <View style={{ gap: 5, marginTop: spacingX._20 }}>
           <Typo size={30} fontWeight={800}>
-            Hey
+            Hey,
           </Typo>{" "}
           <Typo size={30} fontWeight={800}>
             Welcome Back
@@ -30,6 +46,7 @@ const Login = () => {
           </Typo>
           <Input
             placeholder="Enter your email"
+            onChangeText={(value) => (emailRef.current = value)}
             icon={
               <Icons.At
                 size={verticalScale(26)}
@@ -38,6 +55,38 @@ const Login = () => {
               />
             }
           />
+          <Input
+            placeholder="Enter your password"
+            secureTextEntry
+            onChangeText={(value) => (passwordRef.current = value)}
+            icon={
+              <Icons.Lock
+                size={verticalScale(26)}
+                color={colors.neutral300}
+                weight="fill"
+              />
+            }
+          />
+          <Typo size={14} color={colors.text} style={{ alignSelf: "flex-end" }}>
+            Forgot Password
+          </Typo>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <Button loading={isLoading} onPress={handleSubmit}>
+            <Typo fontWeight={"700"} color={colors.black} size={21}>
+              Login
+            </Typo>
+          </Button>
+        </View>
+
+        <View style={styles.footer}>
+          <Typo size={15}>Don't have an account ?</Typo>
+          <Pressable onPress={() => router.push("/(auth)/register")}>
+            <Typo size={15} fontWeight={"700"} color={colors.primary}>
+              Sign up
+            </Typo>
+          </Pressable>
         </View>
       </View>
     </ScreenWrapper>
@@ -49,7 +98,7 @@ export default Login;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    gap: spacingY._30,
+    gap: spacingY._20,
     paddingHorizontal: spacingX._20,
   },
   welcomeText: {
@@ -60,19 +109,23 @@ const styles = StyleSheet.create({
   form: {
     gap: spacingY._20,
   },
+  buttonContainer: {
+    marginTop: spacingY._20,
+  },
   header: {
     paddingTop: spacingY._7,
   },
   forgotPassword: {
     textAlign: "right",
     fontWeight: "500",
-    colors: colors.text,
+    color: colors.text,
   },
   footer: {
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
     gap: 5,
+    marginTop: spacingY._15,
   },
   footerText: {
     textAlign: "center",
